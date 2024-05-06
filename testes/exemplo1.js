9ae2aa902898c3e865ab366af26da3f7204b5371
helders <helders@motorola.com>
2024-05-06 18:21:43 -0400

    gera relatório em html
    Signed-off-by: helders <helders@motorola.com>

--- testes/exemplo1.js
+++ testes/exemplo1.js
@@ -1,5 +1,7 @@
 import http from "k6/http";
 import {check} from "k6";
+import {htmlReport} from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
 
 /*
 * Critérios:
@@ -22,4 +24,10 @@ export default function () {
     const BASE_URL = "https://test-api.k6.io/public/crocodiles/";
     const response = http.get(BASE_URL);
     check(response, {"status code 200": (r) => r.status === 200});
+}
+
+export function handleSummary(data) {
+    return {
+        "summary.html": htmlReport(data),
+    };
 }
